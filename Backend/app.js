2d7ec3a6eb357cb87d88327c13deab682c692289
Soumya Sinha <soumya.sinha0011@gmail.com>
2026-08-19 23:03:03 +0530

    deployment

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ let port =process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors({
     
-    origin: "https://i-note-amber.vercel.app/",
+    origin: "https://i-note-amber.vercel.app",
     credentials: true
 }));
 
