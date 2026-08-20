974a086d2eae6e05c412a51edc76f4b75abfd3d9
Soumya Sinha <soumya.sinha0011@gmail.com>
2026-08-20 18:23:31 +0530

    Fix CORS configuration

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,8 @@ let port =process.env.PORT || 5000;
 const cors = require("cors");
 
 app.use(cors({
-    origin: "https://i-note-amber.vercel.app",
+    origin:[ "https://i-note-amber.vercel.app",
+             "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -37,4 +38,6 @@ app.get('/',(req,res)=>{
     res.send("Hello Guys")
 })
 
-app.listen(port)
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
